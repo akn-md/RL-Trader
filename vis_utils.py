1779b649abd635e12a818da2ea2fc5e7cdfaea3f
Ashwin Nayak <anayak247@gmail.com>
2020-05-25 01:01:17 -0700

    Added vis file

---
+++ vis_utils.py
@@ -0,0 +1,49 @@
+import plotly.graph_objects as go
+
+
+def plot_trades(data, buys, sells):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index, y=data.open))
+
+    buys_x = list(list(zip(*buys))[0])
+    buys_y = list(list(zip(*buys))[1])
+    sells_x = list(list(zip(*sells))[0])
+    sells_y = list(list(zip(*sells))[1])
+    fig.add_trace(
+        go.Scatter(x=buys_x,
+                   y=buys_y,
+                   mode='markers',
+                   name='buys',
+                   fillcolor='royalblue'))
+    fig.add_trace(
+        go.Scatter(x=sells_x,
+                   y=sells_y,
+                   mode='markers',
+                   name='sells',
+                   fillcolor='firebrick'))
+
+    fig.show()
+
+
+def get_trade_plot(data, buys, sells):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index, y=data.open))
+
+    buys_x = list(list(zip(*buys))[0])
+    buys_y = list(list(zip(*buys))[1])
+    sells_x = list(list(zip(*sells))[0])
+    sells_y = list(list(zip(*sells))[1])
+    fig.add_trace(
+        go.Scatter(x=buys_x,
+                   y=buys_y,
+                   mode='markers',
+                   name='buys',
+                   fillcolor='royalblue'))
+    fig.add_trace(
+        go.Scatter(x=sells_x,
+                   y=sells_y,
+                   mode='markers',
+                   name='sells',
+                   fillcolor='firebrick'))
+
+    return fig
